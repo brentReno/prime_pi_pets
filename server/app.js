0de1c38d63a7e414a4caccbfe480035b5cebc901
Brent Reno <brent.reno@gmail.com>
2016-09-23 08:28:47 -0500

    server is serving index.html

--- server/app.js
+++ server/app.js
@@ -8,4 +8,14 @@ app.set("port", (process.env.PORT || 3005));
 //spin up the server
 app.listen(app.get("port"), function(){
   console.log("I'm listening.... on:", app.get("port"));
-});
+});//end spin up server
+
+//catch all route
+app.get("/*", function(req, res){
+  console.log("In the catch all route");
+  console.log("Here is the property:", req.params[0]);
+  //if param 0 has no value, send the index
+  var file = req.params[0] || "/views/index.html";
+  //stringthat points to necessary file
+  res.sendFile(path.join(__dirname, "/public/", file));
+});//end catch all
