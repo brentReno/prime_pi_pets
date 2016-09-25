5369714876ab44c27c2f2cfc1f0558a3635c1420
Brent Reno <brent.reno@gmail.com>
2016-09-25 18:35:11 -0500

    add pets to the DOM

--- server/public/scripts/controllers/petsController.js
+++ server/public/scripts/controllers/petsController.js
@@ -1,12 +1,14 @@
 myApp.controller("petsController", ['$scope','$http',function($scope,$http){
   console.log(" Pets Page");
-  
+  $scope.allPets = [];
   var onLoad= function(){
     $http({
       method: 'GET',
       url: '/pets',
     }).then(function ( response ){
-      console.log('back from server with:', response);
+      console.log('back from server with:', response.data);
+      $scope.allPets =response.data;
+      console.log('all Pets:',$scope.allPets);
     });// end http Call
   };// end on load
   // call on load
--- toDo.md
+++ toDo.md
@@ -2,11 +2,11 @@ To Do's:
 =========
 * v 0.1 server serves index.html - Done
 * v 0.2 navigation for views - Done
-* v 0.3 home view -Basics
-* v 0.4 add view -form built no on click
+* v 0.3 home view -Basics- need to connect buttons
+* v 0.4 add view -form built -done
 * v 0.5 pets view- save for later
 * v 0.6 pets schema - done
 * v 0.7 get route to view pets- done
 * v 0.8 post route to add pet - done
-* v.0.9 add pets to the DOM
+* v.0.9 add pets to the DOM- done
 * v.1.0 styling
