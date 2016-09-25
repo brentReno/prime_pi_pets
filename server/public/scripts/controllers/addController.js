77ed3b353c062d7fb0a1491ea41f2a50ab079ff4
Brent Reno <brent.reno@gmail.com>
2016-09-25 17:12:17 -0500

    moved form collecting to proper controller

--- server/public/scripts/controllers/addController.js
+++ server/public/scripts/controllers/addController.js
@@ -1,3 +1,16 @@
 myApp.controller("addController", ["$scope", function($scope){
   console.log(" Add Page");
+
+  //get pet info
+  $scope.addPet = function(){
+    console.log('in addPet', $scope);
+
+    var newPet = {
+      petName: $scope.petName,
+      petType: $scope.petType,
+      petAge: $scope.petAge,
+      picLink: $scope.petPic
+    }; // end new item
+    console.log('sending:',newPet);
+  };//end addPet
 }]);
