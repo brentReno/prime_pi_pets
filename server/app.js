17ea520bf3b1142b0573c3617e5488408f01abb3
Brent Reno <brent.reno@gmail.com>
2016-09-25 21:35:32 -0500

    added styling for home view

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var petsRouter = require('./routers/petRouter');
 var mongoose=require('mongoose');
 var mongoURI = "mongodb://localhost:27017/piPets";
 var MongoDB = mongoose.connect(mongoURI).connection;
